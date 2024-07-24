aec4f2197008c31f91559f733f6f9dcda82d05d4
Izabela Leme <izabela.leme@plasutil.com.br>
2024-07-24 11:18:45 -0300

    new project

---
+++ Models/Musica.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ScreenSoundAPI.Models;
+
+internal class Musica
+{
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    public void ExibeDetalhesMusica()
+    {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Música: {Nome}");
+        Console.WriteLine($"Duração: {Duracao / 1000}");
+        Console.WriteLine($"Gênero musical: {Genero}");
+    }
+}
